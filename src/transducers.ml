835ba13f12313b9cb0b7757b8ac8bb6942d9b2c4
Rizo Isrof <id@rizo.me>
2015-12-17 02:20:02 +0000

    Update transducers.ml

--- src/transducers.ml
+++ src/transducers.ml
@@ -9,20 +9,18 @@ type ('a, 'b, 'r) transducer = ('b, 'r) reducer -> ('a, 'r) reducer
 (** [transducer] take a reducing function and returns another reducing
     function. *)
 
-let compose t1 t2 = fun r -> t1 (t2 r)
-
-let conj = flip cons
-
 let transduce_list t xs = List.rev (List.fold xs ~init:[] ~f:(t (flip cons)))
 
 let map : ('a -> 'b) -> ('r -> 'b -> 'r) -> ('r -> 'a -> 'r) =
-  fun f k r a -> k r (f a)
+  fun f step r a -> step r (f a)
 
 let filter : ('a -> bool) -> ('r -> 'a -> 'r) -> ('r -> 'a -> 'r) =
-  fun p k r a -> if p a then k r a else r
+  fun p step r a -> if p a then step r a else r
+
+let (<<) f g = fun x -> f (g x)
 
 let () =
-  let plan = map ((+) 10) % filter odd in
+  let plan = map ((+) 10) << filter odd in
   let res  = transduce_list plan (List.range 0 100) in
   print (fmt "#res = %d" (List.length res))
 
